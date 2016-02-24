ce5704f4c94776cecedd02f6a6d191acf701c72b
Paul <pauloptimised@users.noreply.github.com>
2016-02-24 11:46:28 +0000

    Amend static assets

--- app/mailers/challenge_mailer.rb
+++ app/mailers/challenge_mailer.rb
@@ -1,8 +1,8 @@
 class ChallengeMailer < ActionMailer::Base
-  default :from => SITE_SETTINGS[:Email]
-  
+  default from: SITE_SETTINGS['Email']
+
   def challenge_friend(challenge)
     @challenge = challenge
-    mail(:to => challenge.email, :subject => "You have been challenged")
+    mail(to: challenge.email, subject: 'You have been challenged')
   end
 end
